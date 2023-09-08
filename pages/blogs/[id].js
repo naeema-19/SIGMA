67c22ea0ffe81072e2020604acdc23cbd05791b2
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2023-09-08 11:50:22 +0530

    Update [id].js

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -5,7 +5,7 @@ import Head from "next/head"
 import { useRouter } from "next/router"
 import React from "react"
 
-const SinglePost = () => {
+{/*const SinglePost = () => {
   const router = useRouter()
   const { id } = router.query
   const post = blogdata.find((post) => post.id === parseInt(id))
@@ -49,4 +49,28 @@ const SinglePost = () => {
   )
 }
 
+export default SinglePost*/}
+const SinglePost = () => {
+  const router = useRouter()
+  const { id } = router.query
+  const post = blogdata.find((post) => post.id === parseInt(id))
+
+  // Check if post is defined, and if not, display an error message or redirect to a 404 page.
+  if (!post) {
+    // You can handle the error here, e.g., by displaying a message or redirecting.
+    return (
+      <div>
+        <p>Post not found.</p>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      {/* The rest of your component code */}
+    </>
+  )
+}
+
 export default SinglePost
+
